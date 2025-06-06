e0e12d41ad35bce2566a3280f1f85b261e0882f2
ddewsndo <drewsando73@gmail.com>
2025-06-05 21:02:23 -0400

    api key set

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+API_KEY = '9a77dbe6a1c28e3e8a5c8c085c45c3a7'
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+print("\n\nHi! Welcome to my weather app. Enter a city you would like to know the weather for!\n")
+
+userCity = input("Enter city: ")
